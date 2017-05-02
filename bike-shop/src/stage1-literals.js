b476f6d35742ccdff35dd204fd4122977ed60c57
MsMishi <mcwcharles@gmail.com>
2017-05-02 11:16:43 -0700

    Add stage one and day one exercises

--- bike-shop/src/stage1-literals.js
+++ bike-shop/src/stage1-literals.js
@@ -1,5 +1,16 @@
 const myBike = {
   // your code here
+  name: "Roadster",
+  price: 199.99,
+  frame: {
+    height: 55,
+    color: 'red',
+    style: 'mountain'
+  },
+  brakes: {
+    front:false,
+    rear: true,
+  }
 }
 
 module.exports = myBike
